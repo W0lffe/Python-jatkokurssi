41f5df30ac0322c30a8732f96868b3a5bbd0b585
Henry <henry.karppinen1997@gmail.com>
2026-04-25 15:41:40 +0300

    finished SQL

--- M12_SQL/src/my_code.py
+++ M12_SQL/src/my_code.py
@@ -1,6 +1,26 @@
-import sqlite3
+import sqlite3 as sql
 
 db_file='my_db.db'
 
-#Write your code here!
+mydb = sql.connect(db_file)
+
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS texttable (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+"""
+create_insert_query = """
+INSERT INTO texttable (name)
+VALUES ('Matti'), ('Ville'), ('Kaisa'), ('Mikko');
+"""
+
+cursor = mydb.cursor()
+cursor.execute(create_table_query)
+cursor.execute(create_insert_query)
+
+mydb.commit()
+cursor.close()
+mydb.close()
 
--- M13_SQL/src/my_code.py
+++ M13_SQL/src/my_code.py
@@ -1,6 +1,14 @@
-import sqlite3
+import sqlite3 as sql
 
 db_file='my_db.db'
 
-#Write your code here
+mydb = sql.connect(db_file)
 
+select_query = """
+SELECT * FROM texttable;
+"""
+
+for row in mydb.execute(select_query):
+    print(row)
+
+mydb.close()
